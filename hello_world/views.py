95debb3fd3a9d4d478ec7201d7bb88aafcb0485f
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-11-23 17:14:41 +0100

    Poprawiono imię na 'Mikolaj'

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,9 +1,9 @@
 from hello_world import app
-from hello_world.formater import get_formatted
+from hello_world.formatter import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Mikłolaj"
+moje_imie = "Mikolaj"
 msg = "Hello World!"
 
 @app.route('/')
@@ -11,9 +11,9 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+    return get_formatted(msg, moje_imie, output.lower())
 
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
+
