79ea357f768de3450914a0e6b73b9df684c7574d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-11-23 18:11:15 +0100

    Add formater alias module for tests

---
+++ hello_world/formater.py
@@ -0,0 +1,16 @@
+from .formatter import (
+
+    SUPPORTED,
+
+    PLAIN,
+
+    get_formatted,
+
+    plain_text_upper_case,
+
+)
+
+
+
+__all__ = ["SUPPORTED", "PLAIN", "get_formatted", "plain_text_upper_case"]
+
